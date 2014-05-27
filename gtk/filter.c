11fcb7e3df9142eb868cc129daa9987144a0d2df
Derek Willian Stavis <dekestavis@gmail.com>
2014-05-27 01:14:34 -0300

    Add placeholder text to search

--- gtk/filter.c
+++ gtk/filter.c
@@ -311,6 +311,7 @@ gtr_filter_bar_new (tr_session   * session,
   
   /* add the entry field */
   w = gtk_search_entry_new ();
+  gtk_entry_set_placeholder_text (GTK_ENTRY (w), "Search by torrent name");
   g_signal_connect (w, "icon-release", G_CALLBACK (entry_clear), NULL);
   
   gtk_box_pack_start (GTK_BOX (wrapper), w, TRUE, TRUE, 0);
